10e36d540db2f8397f59a14d4883964a10883a01
Maxim Tarasov <tosik4302@mail.ru>
2025-05-15 11:31:15 +0500

    chore: add env var for backup filename prefix, add prefixes to variables, add exception if file not found, rename filename to backup_filename, change IMAGE_TAG to image.tag, template function for namespace, cronjob name and container name, remove unnecessary code, decrease successfulJobsHistoryLimit to 1

--- pgsql-dump.py
+++ pgsql-dump.py
@@ -5,27 +5,32 @@ from datetime import datetime
 
 def main():
 
-    filename = 'pelican-local-env-db-' + datetime.strftime(datetime.utcnow(), "%Y-%m-%dT%H-%M-%S") + '.backup'
+    backup_filename = os.getenv('PELICAN_backup_filename_PREFIX') + '-' + datetime.strftime(datetime.utcnow(), "%Y-%m-%dT%H-%M-%S") + '.backup'
 
-    os.system('pg_dump -h $PG_HOST -U $PG_USER --encoding UTF8 --format plain $PG_DATABASE > %s' %(filename))
-    if os.path.exists(filename):
-        upload_to_s3(filename)
-        os.remove(filename)
+    os.system('pg_dump -h $PG_HOST -U $PG_USER --encoding UTF8 --format plain $PG_DATABASE > %s' %(backup_filename))
 
-def upload_to_s3(filename):
+    if os.path.exists(backup_filename):
+        upload_to_s3(backup_filename)
+        os.remove(backup_filename)
+
+    else:
+        raise Exception("No such file: '%s'" %(backup_filename))
+
+
+def upload_to_s3(backup_filename):
 
     s3 = boto3.client(
         's3',
-        aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-        aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
-        endpoint_url=os.getenv('AWS_HOST'),
+        aws_access_key_id=os.getenv('DESTINATION_AWS_ACCESS_KEY_ID'),
+        aws_secret_access_key=os.getenv('DESTINATION_AWS_SECRET_ACCESS_KEY'),
+        endpoint_url=os.getenv('DESTINATION_AWS_HOST'),
     )
 
-    bucket_name = os.getenv('AWS_BUCKET_NAME')
+    bucket_name = os.getenv('DESTINATION_AWS_BUCKET_NAME')
 
 
-    with open(filename, "rb") as data:
-        s3.upload_fileobj(data, bucket_name, filename)
+    with open(backup_filename, "rb") as data:
+        s3.upload_fileobj(data, bucket_name, backup_filename)
 
 if __name__ == '__main__':
 
