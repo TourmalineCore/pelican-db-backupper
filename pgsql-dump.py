776c23102fff42aa5f6fb7f034546128bef63189
Yam1x <rychkov03@yandex.ru>
2025-05-22 13:55:41 +0500

    fix!: rename env variables to save similar naming between services

--- pgsql-dump.py
+++ pgsql-dump.py
@@ -7,7 +7,7 @@ def main():
 
     backup_filename = os.getenv('DB_BACKUP_FILENAME_PREFIX') + '-' + datetime.strftime(datetime.utcnow(), "%Y-%m-%dT%H-%M-%S") + '.backup'
 
-    os.system('pg_dump -h $PG_HOST -U $PG_USER --encoding UTF8 --format plain $PG_DATABASE > %s' %(backup_filename))
+    os.system('pg_dump -h $DATABASE_HOST -U $DATABASE_USERNAME --encoding UTF8 --format plain $DATABASE_NAME > %s' %(backup_filename))
 
     if os.path.exists(backup_filename):
         upload_to_s3(backup_filename)
@@ -21,12 +21,12 @@ def upload_to_s3(backup_filename):
 
     s3 = boto3.client(
         's3',
-        aws_access_key_id=os.getenv('DESTINATION_DB_AWS_ACCESS_KEY_ID'),
-        aws_secret_access_key=os.getenv('DESTINATION_DB_AWS_SECRET_ACCESS_KEY'),
-        endpoint_url=os.getenv('DESTINATION_DB_AWS_HOST'),
+        aws_access_key_id=os.getenv('BACKUP_AWS_ACCESS_KEY_ID'),
+        aws_secret_access_key=os.getenv('BACKUP_AWS_SECRET_ACCESS_KEY'),
+        endpoint_url=os.getenv('BACKUP_AWS_HOST'),
     )
 
-    bucket_name = os.getenv('DESTINATION_DB_AWS_BUCKET_NAME')
+    bucket_name = os.getenv('BACKUP_AWS_BUCKET_NAME')
 
 
     with open(backup_filename, "rb") as data:
