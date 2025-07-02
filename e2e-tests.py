75c91d6f305df9c487b37e1c4e7857f049f9e40f
Yam1x <rychkov03@yandex.ru>
2025-07-02 16:54:52 +0500

    docs: comms for checks

--- e2e-tests.py
+++ e2e-tests.py
@@ -11,6 +11,7 @@ def main():
 
     objects_list = s3.list_objects_v2(Bucket=os.getenv('DB_AWS_BUCKET_NAME'))
 
+    # Checking that bucket isn't empty
     try:
         contents = objects_list["Contents"]
 
@@ -23,10 +24,11 @@ def main():
         if content["Key"].startswith(os.getenv('DB_BACKUPS_FILENAME_PREFIX')):
             backups.append(content)
 
+    # Checking that database backups exists in bucket
     if len(backups) == 0:
         raise Exception("No database backups found")
 
-
+    # Checking that database backup file size isn't zero 
     if backups[-1]["Size"] == 0:
         raise Exception("Backup size is 0")
 
